198923852626cccd2c47e7ec5ab035d0407e4f0a
Adrián <47431198+fmadrian@users.noreply.github.com>
2025-01-21 13:20:13 -0600

    PrimeNG installed and integrated with Tailwind CSS [PUEBLA-19]

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
 import { provideRouter } from '@angular/router';
+import Material from '@primeng/themes/material';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Material,
+        options: {
+          darkModeSelector: false,
+        },
+      }
+    })
+  ]
 };
