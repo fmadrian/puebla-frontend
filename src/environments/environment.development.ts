4cf9be59b53edd425694ffdaceb53bb7d2f00708
Adrián <47431198+fmadrian@users.noreply.github.com>
2025-01-22 12:48:33 -0600

    Environments added [PUEBLA-19]

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+const API_VERSION = "1";
+export const environment = {
+    API_URL: `http://localhost:5006/api/v${API_VERSION}`,
+    RECOVERY_PASSWORD_EMAIL_ADDRESS: '',
+    CLIENT_ADDRESS: 'http://localhost:4200',
+};
